3fd4092ff07dc7a8b7f2f7589f57800dad9582a6
minjae-chowis <mjkim@chowis.com>
2024-06-24 13:37:32 +0900

    sync consultants entity with dior login portal v2

--- src/common/entities/crmEntities/Consultants.entity.ts
+++ src/common/entities/crmEntities/Consultants.entity.ts
@@ -31,231 +31,216 @@ import { Products } from './Products.entity';
 @Entity('consultants', { schema: 'public' })
 export class Consultants {
     @PrimaryGeneratedColumn({ type: 'bigint', name: 'id' })
-    id: number;
+    'id': number;
 
     @Column('bigint', {
         name: 'consultant_company_id',
         nullable: true,
         default: () => '1',
     })
-    consultant_company_id: number | null;
+    'consultant_company_id': number | null;
 
     @Column('bigint', { name: 'consultant_branch_id', nullable: true })
-    consultant_branch_id: number | null;
+    'consultant_branch_id': number | null;
 
     @Column('bigint', { name: 'consultant_shop_id', nullable: true })
-    consultant_shop_id: number | null;
+    'consultant_shop_id': number | null;
 
     @Column('bigint', {
         name: 'consultant_position_id',
         nullable: true,
         default: () => '4',
     })
-    consultant_position_id: number | null;
+    'consultant_position_id': number | null;
 
     @Column('character varying', { name: 'token', nullable: true })
-    token: string | null;
+    'token': string | null;
 
     @Column('character varying', { name: 'email', nullable: true })
-    email: string | null;
+    'email': string | null;
 
     @Column('character varying', { name: 'password_digest', nullable: true })
-    password_digest: string | null;
+    'password_digest': string | null;
 
     @Column('character varying', {
         name: 'recovery_password_digest',
         nullable: true,
     })
-    recovery_password_digest: string | null;
+    'recovery_password_digest': string | null;
 
     @Column('character varying', { name: 'social', nullable: true })
-    social: string | null;
+    'social': string | null;
 
     @Column('character varying', { name: 'social_id', nullable: true })
-    social_id: string | null;
+    'social_id': string | null;
 
     @Column('character varying', { name: 'name', nullable: true })
-    name: string | null;
+    'name': string | null;
 
     @Column('character varying', { name: 'phone', nullable: true })
-    phone: string | null;
+    'phone': string | null;
 
     @Column('text', { name: 'address', nullable: true })
-    address: string | null;
+    'address': string | null;
 
     @Column('text', { name: 'note', nullable: true })
-    note: string | null;
+    'note': string | null;
 
     @Column('text', { name: 'push_token', nullable: true })
-    push_token: string | null;
+    'push_token': string | null;
 
     @Column('boolean', {
         name: 'approved',
         nullable: true,
         default: () => 'false',
     })
-    approved: boolean | null;
+    'approved': boolean | null;
 
     @Column('character varying', { name: 'surname', nullable: true })
-    surname: string | null;
+    'surname': string | null;
+
+    @Column('character varying', { name: 'surname', nullable: true })
+    'gender': string | null;
 
     @Column('character varying', { name: 'birthdate', nullable: true })
-    birthdate: string | null;
+    'birthdate': string | null;
 
     @Column('character varying', { name: 'city', nullable: true })
-    city: string | null;
+    'city': string | null;
 
-    @Column('character varying', { name: 'country_name', nullable: true })
-    country_name: string | null;
+    @Column('text', {
+        name: 'country',
+        nullable: true,
+        array: true,
+        default: () => "'{}'[]",
+    })
+    'country': string[] | null;
 
     @Column('integer', { name: 'app_id', nullable: true })
-    app_id: number | null;
+    'app_id': number | null;
+
+    @Column('bigint', { name: 'consultant_country_id', nullable: true })
+    'consultant_country_id': number | null;
 
     @Column('bigint', { name: 'consultant_store_id', nullable: true })
-    consultant_store_id: number | null;
+    'consultant_store_id': number | null;
 
     @Column('boolean', { name: 'email_confirmed', nullable: true })
-    email_confirmed: boolean | null;
+    'email_confirmed': boolean | null;
 
     @Column('character varying', { name: 'confirm_token', nullable: true })
-    confirm_token: string | null;
+    'confirm_token': string | null;
 
     @Column('character varying', { name: 'os', nullable: true })
-    os: string | null;
+    'os': string | null;
 
     @Column('character varying', { name: 'language', nullable: true })
-    language: string | null;
+    'language': string | null;
 
     @Column('timestamp without time zone', { name: 'created_at' })
-    created_at: Date;
+    'created_at': Date;
 
     @Column('timestamp without time zone', { name: 'updated_at' })
-    updated_at: Date;
+    'updated_at': Date;
 
     @Column('character varying', { name: 'memo', nullable: true })
-    memo: string | null;
+    'memo': string | null;
 
     @Column('character varying', { name: 'company_name', nullable: true })
-    company_name: string | null;
+    'company_name': string | null;
 
     @Column('character varying', { name: 'company_address', nullable: true })
-    company_address: string | null;
+    'company_address': string | null;
 
     @Column('character varying', { name: 'position', nullable: true })
-    position: string | null;
+    'position': string | null;
 
     @Column('character varying', { name: 'branch', nullable: true })
-    branch: string | null;
+    'branch': string | null;
 
     @Column('character varying', { name: 'zip_code', nullable: true })
-    zip_code: string | null;
+    'zip_code': string | null;
 
     @Column('character varying', { name: 'state', nullable: true })
-    state: string | null;
+    'state': string | null;
 
     @Column('integer', { name: 'skin_color_group_id', nullable: true })
-    skin_color_group_id: number | null;
+    'skin_color_group_id': number | null;
 
     @Column('integer', { name: 'ethnicity_id', nullable: true })
-    ethnicity_id: number | null;
+    'ethnicity_id': number | null;
 
     @Column('integer', { name: 'status', nullable: true, default: () => '0' })
-    status: number | null;
+    'status': number | null;
 
     @Column('character varying', { name: 'callback_url', nullable: true })
-    callback_url: string | null;
+    'callback_url': string | null;
 
     @Column('integer', { name: 'is_active', nullable: true })
-    is_active: number | null;
+    'is_active': number | null;
 
     @Column('character varying', { name: 'code', nullable: true })
-    code: string | null;
+    'code': string | null;
 
     @Column('character varying', { name: 'otp_token', nullable: true })
-    otp_token: string | null;
+    'otp_token': string | null;
 
     @Column('timestamp without time zone', {
         name: 'otp_valid_til',
         nullable: true,
     })
-    otp_valid_til: Date | null;
-
-    @Column('text', {
-        name: 'countries',
-        nullable: true,
-        array: true,
-        default: () => "'{}'[]",
-    })
-    countries: string[] | null;
+    'otp_valid_til': Date | null;
 
     @Column('timestamp without time zone', {
         name: 'confirmation_sent_at',
         nullable: true,
     })
-    confirmation_sent_at: Date | null;
+    'confirmation_sent_at': Date | null;
 
     @Column('timestamp without time zone', {
         name: 'confirmed_at',
         nullable: true,
     })
-    confirmed_at: Date | null;
+    'confirmed_at': Date | null;
 
     @Column('character varying', { name: 'unconfirmed_email', nullable: true })
-    unconfirmed_email: string | null;
+    'unconfirmed_email': string | null;
 
-    @Column('boolean', {
-        name: 'register_for_crm',
-        nullable: true,
-        default: () => 'false',
-    })
-    register_for_crm: boolean | null;
+    @Column('boolean', { name: 'password_update_needed', nullable: true })
+    'password_update_needed': boolean | null;
 
-    @Column('character varying', { name: 'phone_country_code', nullable: true })
-    phone_country_code: string | null;
+    @Column('boolean', { name: 'hide_for_bc', nullable: true })
+    'hide_for_bc': boolean | null;
 
-    @Column('boolean', {
-        name: 'email_subscription',
-        nullable: true,
-        default: () => 'true',
-    })
-    email_subscription: boolean | null;
-
-    @Column('integer', { name: 'country_id', nullable: true })
-    country_id: number;
-
-    @ManyToOne(() => ConsultantShops, (consultantShops) => consultantShops.consultants, {
+    @ManyToOne(() => ConsultantShops, (consultantShops: ConsultantShops) => consultantShops.consultants, {
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE',
     })
     @JoinColumn([{ name: 'consultant_shop_id', referencedColumnName: 'id' }])
-    consultant_shop: ConsultantShops;
+    'consultant_shop': ConsultantShops;
 
-    @ManyToOne(() => Countries, (countries) => countries.consultants, {
+    @ManyToOne(() => Countries, (countries: Countries) => countries.consultants, {
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE',
     })
-    @JoinColumn([{ name: 'country_id', referencedColumnName: 'id' }])
-    country_details: Countries;
+    @JoinColumn([{ name: 'consultant_country_id', referencedColumnName: 'id' }])
+    'country_details': Countries;
 
-    @ManyToOne(() => Genders, (genders) => genders.consultants)
-    @JoinColumn([{ name: 'gender_id', referencedColumnName: 'id' }])
-    gender: Genders;
+    @OneToMany(() => Customers, (customers: Customers) => customers.conslutant)
+    'customers': Customers[];
 
-    @OneToMany(() => Customers, (customers) => customers.conslutant)
-    customers: Customers[];
+    @OneToMany(() => Products, (products: Products) => products.consultant)
+    'products': Products[];
 
-    @OneToMany(() => Products, (products) => products.consultant)
-    products: Products[];
-
-    @OneToOne(() => ConsultantPositions, (consultantPositions) => consultantPositions.consultant)
+    @OneToOne(() => ConsultantPositions, (consultantPositions: ConsultantPositions) => consultantPositions.consultant)
     @JoinColumn([{ name: 'consultant_position_id', referencedColumnName: 'id' }])
-    consultant_position: ConsultantPositions;
+    'consultant_position': ConsultantPositions;
 
-    @OneToOne(() => ConsultantCompanies, (consultantCompanies) => consultantCompanies.consultant)
+    @OneToOne(() => ConsultantCompanies, (consultantCompanies: ConsultantCompanies) => consultantCompanies.consultant)
     @JoinColumn([{ name: 'consultant_company_id', referencedColumnName: 'id' }])
-    consultant_company: ConsultantCompanies;
+    'consultant_company': ConsultantCompanies;
 
-    get getOpticNumbers(): string[] | null {
+    get 'getOpticNumbers'(): string[] | null {
         if (this.products && this.products.length > 0) {
             // Map each product's optic_number to an array
             return this.products.map((product) => product.device.optic_number);
@@ -263,21 +248,21 @@ export class Consultants {
         return [];
     }
 
-    get getContryCode(): string | null {
+    get 'getContryCode'(): string | null {
         if (this.country_details) {
             return this.country_details.country_code;
         }
         return null;
     }
 
-    get getContryName(): string | null {
+    get 'getContryName'(): string | null {
         if (this.country_details) {
             return this.country_details.name;
         }
         return null;
     }
 
-    get getSerialNumbers(): string[] | null {
+    get 'getSerialNumbers'(): string[] | null {
         if (this.products && this.products.length > 0) {
             // Map each product's serial_number to an array
             return this.products.map((product) => product.device.serial_number);
@@ -285,7 +270,7 @@ export class Consultants {
         return [];
     }
 
-    get getStoreName(): string | null {
+    get 'getStoreName'(): string | null {
         if (this.consultant_shop) {
             return this.consultant_shop.name;
         }
@@ -293,9 +278,8 @@ export class Consultants {
     }
 
     @AfterLoad()
-    afterLoad() {
+    'afterLoad'() {
         this.id = Number(this.id);
-        if (this.country_id) this.country_id = Number(this.country_id);
         if (this.consultant_company_id) this.consultant_company_id = Number(this.consultant_company_id);
         if (this.consultant_branch_id) this.consultant_branch_id = Number(this.consultant_branch_id);
         if (this.consultant_shop_id) this.consultant_shop_id = Number(this.consultant_shop_id);
