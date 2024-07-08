6106dd1749dc445fead634bf131b2df2eb1a4b06
minjae-chowis <mjkim@chowis.com>
2024-07-08 10:32:17 +0900

    seperate databse module at app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,6 +46,7 @@ import { AllExceptionsFilter } from './common/middleWare/exceptions/exceptionHan
 import { LoggingMiddleware } from './common/middleWare/logMiddleWare/logging.middleware';
 
 import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
     imports: [
@@ -107,100 +108,10 @@ import { DevtoolsModule } from '@nestjs/devtools-integration';
         DevtoolsModule.register({
             http: process.env.NODE_ENV !== 'production',
         }),
-        /* GLOBAL DB */
-        // CRM
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('globalDB'),
-                autoLoadEntities: true,
-            }),
-        }),
 
-        // cndp SKIN
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'cndpSkinDB',
-            useFactory: async (configService: ConfigService) => ({ ...configService.get('cndpSkinDB') }),
-        }),
+        //
+        DatabaseModule,
 
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'cndpHairDB',
-            useFactory: async (configService: ConfigService) => ({ ...configService.get('cndpHairDB') }),
-        }),
-
-        /* OHIO DB */
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'secondDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('secondDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-
-        //analysis DB
-        // CNDP SKIN ANALYSIS
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'ohioCndpSkinDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('ohioCndpSkinDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-        // CNDP HAIR ANALYSIS
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'ohioCndpHairDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('ohioCndpHairDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-
-        // third DATABASE
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'thirdDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('thirdDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-
-        //analysis DB
-        // CNDP SKIN ANALYSIS
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'thirdCndpSkinDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('thirdCndpSkinDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-        // CNDP HAIR ANALYSIS
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            name: 'thirdCndpHairDB',
-            useFactory: async (configService: ConfigService) => ({
-                ...configService.get('thirdCndpHairDB'),
-                autoLoadEntities: true,
-            }),
-        }),
-        // ServeStaticModule.forRoot({
-        //     rootPath: join(__dirname, '..', 'public'),
-        // }),
         ConsultantsModule,
         CustomersModule,
         DiorModule,
---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,106 @@
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+
+@Module({
+    imports: [
+        /* GLOBAL DB */
+        // CRM
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('globalDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+
+        // cndp SKIN
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'cndpSkinDB',
+            useFactory: async (configService: ConfigService) => ({ ...configService.get('cndpSkinDB') }),
+        }),
+
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'cndpHairDB',
+            useFactory: async (configService: ConfigService) => ({ ...configService.get('cndpHairDB') }),
+        }),
+
+        /* OHIO DB */
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'secondDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('secondDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+
+        //analysis DB
+        // CNDP SKIN ANALYSIS
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'ohioCndpSkinDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('ohioCndpSkinDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+        // CNDP HAIR ANALYSIS
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'ohioCndpHairDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('ohioCndpHairDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+
+        // third DATABASE
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'thirdDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('thirdDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+
+        //analysis DB
+        // CNDP SKIN ANALYSIS
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'thirdCndpSkinDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('thirdCndpSkinDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+        // CNDP HAIR ANALYSIS
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            name: 'thirdCndpHairDB',
+            useFactory: async (configService: ConfigService) => ({
+                ...configService.get('thirdCndpHairDB'),
+                autoLoadEntities: true,
+            }),
+        }),
+    ],
+})
+export class DatabaseModule {}
