5a04e38f77d1cd997eca5bc5c48bfa618a0cce69
minjae-chowis <mjkim@chowis.com>
2024-07-31 12:07:07 +0900

    auto recommend e2e test

---
+++ test/dior/productRecommendations.e2e-spec.ts
@@ -0,0 +1,62 @@
+import * as request from 'supertest';
+import { consultantLoginData, consultantUpdateData } from '../utils/data';
+import { findMissingFields } from '../utils/helper';
+import axios from 'axios';
+
+const localUrl = 'localhost:3100/v1/api';
+const rubyUrl = 'https://stg-dior.chowis.cloud/api';
+
+let localToken: string;
+let rubyToken: string;
+
+describe('Dior - Product Recommendations Module (e2e)', () => {
+    beforeAll(async () => {
+        const localResponse = await request(localUrl).post('/consultants/login').send(consultantLoginData);
+        const rubyResponse = await request(rubyUrl).post('/consultants/login').send(consultantLoginData);
+
+        rubyToken = rubyResponse.body.token;
+        localToken = localResponse.body.token;
+    });
+
+    test('dior/product_recommendations (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get('/dior/product_recommendations')
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get('/dior/product_recommendations')
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    });
+
+    test('dior/product_recommendations/get_automatic_product_by_batch_id (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get(
+                '/dior/product_recommendations/get_automatic_product_by_batch_id?answers=B%2CC%2CB%2CA%2CB%2CB&batch_id=69950&market=Korea&routine_recommendation=5&skin_tone=0.5N',
+            )
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get(
+                '/dior/product_recommendations/get_automatic_product_by_batch_id?answers=B%2CC%2CB%2CA%2CB%2CB&batch_id=69950&market=Korea&routine_recommendation=5&skin_tone=0.5N',
+            )
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    });
+});
