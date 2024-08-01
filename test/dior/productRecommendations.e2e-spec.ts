646efb479f9bc0d65f08cca90248204e74b64a3a
minjae-chowis <mjkim@chowis.com>
2024-08-01 11:21:45 +0900

    GET specifc recommendation e2e test

--- test/dior/productRecommendations.e2e-spec.ts
+++ test/dior/productRecommendations.e2e-spec.ts
@@ -83,6 +83,25 @@ describe('Dior - Product Recommendations Module (e2e)', () => {
         expect(missingFields).toEqual([]);
     });
 
+    test('dior/product_recommendations/:id (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get('/dior/product_recommendations/864')
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get('/dior/product_recommendations/864')
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    });
+
     // test('dior/product_recommendations/get_category (GET)', async () => {
     //     const localResponse = await request(localUrl)
     //         .get('/dior/product_recommendations/get_category?routine=Skincare')
