dc6146ac4c9a2a4c395dcf39d039162bae370c1f
minjae-chowis <mjkim@chowis.com>
2024-08-06 15:13:28 +0900

    fail company branches

---
+++ test/dior/companyBranches.e2e-spec.ts
@@ -0,0 +1,43 @@
+import * as request from 'supertest';
+import { consultantLoginData, consultantUpdateData } from '../utils/data';
+import { findMissingFields } from '../utils/helper';
+import axios from 'axios';
+
+const localUrl = 'localhost:3100/v1/api';
+const rubyUrl = 'https://stg-dior.chowis.cloud/api';
+
+let localToken: string;
+let rubyToken: string;
+
+describe('Dior - Company Branches Module (e2e)', () => {
+    beforeAll(async () => {
+        const localResponse = await request(localUrl).post('/consultants/login').send(consultantLoginData);
+        const rubyResponse = await request(rubyUrl).post('/consultants/login').send(consultantLoginData);
+
+        rubyToken = rubyResponse.body.token;
+        localToken = localResponse.body.token;
+    });
+
+    // test('dior/admins (GET)', async () => {
+    //     const localResponse = await request(localUrl)
+    //         .get('/dior/admins?page=1&per=2')
+    //         .auth(localToken, {
+    //             type: 'bearer',
+    //         })
+    //         .send()
+    //         .expect(200);
+
+    //     const rubyResponse = await request(rubyUrl)
+    //         .get('/dior/admins?page=1&per=2')
+    //         .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+    //         .send()
+    //         .expect(200);
+
+    //     const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+    //     expect(missingFields).toEqual([]);
+    // });
+
+    test('temp', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
