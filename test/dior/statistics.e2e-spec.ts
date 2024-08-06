0f6a9132a5183e2e578586b0a9eb0716dc99eb32
minjae-chowis <mjkim@chowis.com>
2024-08-06 15:32:59 +0900

    add statistics over all e2e test

---
+++ test/dior/statistics.e2e-spec.ts
@@ -0,0 +1,39 @@
+import * as request from 'supertest';
+import { consultantLoginData, consultantUpdateData } from '../utils/data';
+import { findMissingFields } from '../utils/helper';
+import axios from 'axios';
+
+const localUrl = 'localhost:3100/v1/api';
+const rubyUrl = 'https://stg-dior.chowis.cloud/api';
+
+let localToken: string;
+let rubyToken: string;
+
+describe('Dior - Statistics Module (e2e)', () => {
+    beforeAll(async () => {
+        const localResponse = await request(localUrl).post('/consultants/login').send(consultantLoginData);
+        const rubyResponse = await request(rubyUrl).post('/consultants/login').send(consultantLoginData);
+
+        rubyToken = rubyResponse.body.token;
+        localToken = localResponse.body.token;
+    });
+
+    test('dior/statistics/overall (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get('/dior/statistics/overall')
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get('/dior/statistics/overall')
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    });
+});
