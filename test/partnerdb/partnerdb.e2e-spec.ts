f6a23a66807345fe1a03a47b32ef2baaf78c40dd
minjae-chowis <mjkim@chowis.com>
2024-08-08 11:30:57 +0900

    partnerdb e2e test init

---
+++ test/partnerdb/partnerdb.e2e-spec.ts
@@ -0,0 +1,39 @@
+import * as request from 'supertest';
+import { consultantLoginData, consultantUpdateData } from '../utils/data';
+import { findMissingFields } from '../utils/helper';
+import axios from 'axios';
+
+const localUrl = 'localhost:3100/v1/api';
+const rubyUrl = 'https://stg-dior.chowis.cloud/api';
+
+let localToken: string;
+let rubyToken: string;
+
+describe('PartnerDB Module (e2e)', () => {
+    beforeAll(async () => {
+        const localResponse = await request(localUrl).post('/consultants/login').send(consultantLoginData);
+        const rubyResponse = await request(rubyUrl).post('/consultants/login').send(consultantLoginData);
+
+        rubyToken = rubyResponse.body.token;
+        localToken = localResponse.body.token;
+    });
+
+    test('partnerdb/consultants/:id (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get('/partnerdb/consultants/19617')
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get('/partnerdb/consultants/19617')
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    });
+});
