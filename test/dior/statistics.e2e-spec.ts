f378c3cfbca1010d871f6b6ee98c52ea18929d5c
minjae-chowis <mjkim@chowis.com>
2024-08-08 16:52:34 +0900

    dior statistics overall details

--- test/dior/statistics.e2e-spec.ts
+++ test/dior/statistics.e2e-spec.ts
@@ -35,4 +35,23 @@ describe('Dior - Statistics Module (e2e)', () => {
         const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
         expect(missingFields).toEqual([]);
     });
+
+    test('dior/statistics/overall_details (GET)', async () => {
+        const localResponse = await request(localUrl)
+            .get('/dior/statistics/overall_details')
+            .auth(localToken, {
+                type: 'bearer',
+            })
+            .send()
+            .expect(200);
+
+        const rubyResponse = await request(rubyUrl)
+            .get('/dior/statistics/overall_details')
+            .set('X-CHOWIS-CONSULTANT-TOKEN', rubyToken)
+            .send()
+            .expect(200);
+
+        const missingFields = findMissingFields(rubyResponse.body, localResponse.body);
+        expect(missingFields).toEqual([]);
+    }, 30000);
 });
