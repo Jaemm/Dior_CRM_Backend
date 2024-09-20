69404ed0fd47877452131aba6db988acd26668cc
hpkn <hassangpaul@gmail.com>
2024-09-20 10:34:40 +0900

    qr code generate

--- src/modules/utils/utils.module.ts
+++ src/modules/utils/utils.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt/dist';
 import { UtilsController } from './utils.controller';
 import { UtilsService } from './utils.service';
+import { AuthMiddleware } from '@/src/common/middleWare/authMiddlware/auth.middleware';
 
 @Module({
     imports: [],
@@ -9,4 +10,11 @@ import { UtilsService } from './utils.service';
     controllers: [UtilsController],
     exports: [UtilsService],
 })
-export class UtilsModule {}
+export class UtilsModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthMiddleware).forRoutes({
+            path: 'utils/generate_qr_code',
+            method: RequestMethod.GET,
+        });
+    }
+}
